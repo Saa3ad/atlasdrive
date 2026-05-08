c7adbdd6ae7abff954d9f916e32860bf1fe14053
saad <ghalisaad31@gmail.com>
2026-05-08 22:24:27 +0100

    Finish updates for the merge

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import home from '../pages/home.vue'
-import Agencies from '../pages/Agencies.vue'
-import Pricing from '../pages/Pricing.vue'
-import Fleet from '../pages/Fleet.vue'
+import home            from '../pages/home.vue'
+import Agencies        from '../pages/Agencies.vue'
+import Pricing         from '../pages/Pricing.vue'
+import Fleet           from '../pages/Fleet.vue'
+import AgencyDashboard from '../pages/AgencyDashboard.vue'
+import ClientDashboard from '../pages/ClientDashboard.vue'
+import AdminDashboard  from '../pages/AdminDashboard.vue'
 
 const routes = [
-  { path: '/',          name: 'Home',     component: home     },
-  { path: '/agencies',  name: 'Agencies', component: Agencies },
-  { path: '/pricing',   name: 'Pricing',  component: Pricing  },
-  { path: '/fleet',     name: 'Fleet',    component: Fleet    },
+  { path: '/',                name: 'Home',            component: home            },
+  { path: '/agencies',        name: 'Agencies',        component: Agencies        },
+  { path: '/pricing',         name: 'Pricing',         component: Pricing         },
+  { path: '/fleet',           name: 'Fleet',           component: Fleet           },
+  { path: '/dashboard/agency',name: 'AgencyDashboard', component: AgencyDashboard },
+  { path: '/dashboard/client',name: 'ClientDashboard', component: ClientDashboard },
+  { path: '/dashboard/admin', name: 'AdminDashboard',  component: AdminDashboard  },
 ]
 
 const router = createRouter({
